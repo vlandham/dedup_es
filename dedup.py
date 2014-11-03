4d40f66751b158a551456f720bf64c619082e61a
Jim V <vlandham@gmail.com>
2014-11-03 11:57:13 -0800

    test files. modify weights to be independent of num of modules

--- dedup.py
+++ dedup.py
@@ -6,25 +6,34 @@ import sys
 import glob
 import os
 import csv
+import math
+from pprint import pprint
 
-def weight_score(score, module_id):
-  weights = [0.55, 0.30, 0.15, 0.15, 0.15, 0.15, 0.15]
-  return (score * weights[module_id - 1])
+# def weight_score(score, module_id, module_count):
+  # weights = [0.55, 0.30, 0.15, 0.15, 0.15, 0.15, 0.15]
+  # return (score * weights[module_id])
 
-def parse_affinities(customers, filepath, email_id, module_id):
+def generate_weights(module_count):
+  weights = [pow(0.5,i) for i in range(1, module_count + 1)]
+  weights_sum = sum(weights)
+  weights = [weight / weights_sum for weight in weights]
+  return weights
+
+def parse_affinities(customers, filepath, email_id, module_id, module_count):
+  weights = generate_weights(module_count)
   with open(filepath) as f:
     for line in f:
       line = json.loads(line)
-      data = {'id':line["id"], 'score':float(line["norm_score"])}
+      data = {'party_id':line["party_id"], 'score':float(line["nscore"])}
 
-      cust_id = data['id']
+      cust_id = data['party_id']
       if cust_id not in customers:
         customers[cust_id] = {}
 
       if email_id not in customers[cust_id]:
         customers[cust_id][email_id] = {'score':0, 'email':email_id}
 
-      w_score = weight_score(data['score'], module_id)
+      w_score = round(data['score'] * weights[module_id], 4)
       customers[cust_id][email_id]['score'] += w_score
 
   return(customers)
@@ -67,24 +76,32 @@ for email_dir in email_dirs:
 
 
   module_files = glob.glob(email_dir + "/*.out")
+  print "mod count"
+  print len(module_files)
   if len(module_files) == 0:
     print "no modules for " + email_id + " - skipping"
     continue
 
-  for i, module_file in enumerate(module_files):
+  for i, module_file in enumerate(sorted(module_files)):
     print module_file + "\n"
-    # customers = parse_affinities(customers, module_file, email_id, (i + 1))
+    customers = parse_affinities(customers, module_file, email_id, i, len(module_files))
+    print "cust size:"
     print len(customers)
 
 
 
-output_filename = campaign_filepath + "/" + campaign_id + ".tsv"
+output_filename = os.path.abspath(campaign_filepath + "/" + campaign_id + ".tsv")
+print "writing to:"
+print output_filename
 file = open(output_filename, 'w')
-headers = ['id', 'email_id', 'score']
+headers = ['party_id', 'email_id', 'score']
 writer = csv.writer(file, delimiter="\t")
 writer.writerow(headers)
 for cust_id, email_dict in customers.iteritems():
+  # print cust_id
+  # pprint(email_dict)
   top_email = find_top(email_dict.values())
+  # pprint(top_email)
   data = [cust_id, top_email['email'], top_email['score']]
   writer.writerow(data)
 
